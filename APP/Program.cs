51cfd31bfcbac45b1ec4182ee58819bdc4320f76
ademaus <48208485+amaurydelassus@users.noreply.github.com>
2021-10-07 13:32:36 +0200

    autentification : auth0/jwt/autorisation (#3)
    * Auto stash before rebase of "origin/master"
    
    * auth0 : APP
    
    * auth0 : Autorisation

--- APP/Program.cs
+++ APP/Program.cs
@@ -1,12 +1,9 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace APP
 {
@@ -17,7 +14,13 @@ namespace APP
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddOidcAuthentication(options =>
+            {
+                builder.Configuration.Bind("Auth0", options.ProviderOptions);
+                options.ProviderOptions.ResponseType = "code";
+            });
 
             await builder.Build().RunAsync();
         }
